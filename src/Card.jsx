6737a2824ab57e8508bff441d3571d689cdb3a6f
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-14 12:05:06 +0000

    feat: Card Component

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="bg-green-400 text-center">
+      <img alt="robots" src="https://robohash.org/1?200x200" />
+      <h2>Aunuun Jeffry Mahbuubi</h2>
+      <p>jeffryaun@gmail.com</p>
+    </div>
+  );
+}
+
+export default Card;
