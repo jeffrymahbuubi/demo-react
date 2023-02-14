e5022410b5d1a24ca8daee963b7fe1a56653313e
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-14 14:07:59 +0000

    feat: change App

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,6 @@ import Card from "./Card";
 
 const App = () => {
   return (
-    // bg-light-green : background light-green
-    // dib :
-    // br3 : border of 3
-    // pa3 : padding of 3
-    // grow : animation
-    // bw2 : border-widths of 2
-    // shadow-5 : shadow of 5
     <div>
       <h1>RoboFriends</h1>
       <Card />
