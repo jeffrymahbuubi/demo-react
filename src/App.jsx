3dc4ef2efed67b51acea1ece25188d61dd9ca89a
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-14 12:05:13 +0000

    feat: component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./index.css";
+import Card from "./Card";
 
 const App = () => {
   return (
+    // bg-light-green : background light-green
+    // dib :
+    // br3 : border of 3
+    // pa3 : padding of 3
+    // grow : animation
+    // bw2 : border-widths of 2
+    // shadow-5 : shadow of 5
     <div>
-      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        Demo React + Vite
-      </h1>
+      <h1>RoboFriends</h1>
+      <Card />
     </div>
   );
 };
