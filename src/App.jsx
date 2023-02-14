8ed4ec684af38204c6220b7565414d3197d2375a
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-14 09:48:11 +0000

    feat: run prettier to all file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react"
+import React from "react";
+import "./index.css";
 
 const App = () => {
-  return(
+  return (
     <div>
-      <h1>Demo React + Vite</h1>
+      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Demo React + Vite
+      </h1>
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
